396c094b429a9d7f0e7f399f92fe0c39aa7cf1be
Rebecca King <bexking@Rebeccas-MacBook-Pro-2.local>
2019-10-21 00:05:13 +1300

    basically adding the whole website. oops

---
+++ main.js
@@ -0,0 +1,29 @@
+function homeIcon() {
+	let home=document.getElementById("home");
+	home.src = "images/stickfig2.png"
+}
+
+function homeIcon2() {
+	let home=document.getElementById("home");
+	home.src = "images/stickfig.png"
+}
+
+let i=0;
+
+function blocks() {
+	if (i<4){
+		document.getElementsByClassName('blocks')[i].style.fill='black';
+		document.getElementsByClassName('blocks')[i+1].style.fill='red';
+		document.getElementsByClassName('blocks')[i+1].onclick=blocks;
+		i=i+1;
+	} else {
+		document.getElementById('Projects').style.color='red';
+		document.getElementById('Projects').style.fontWeight='700';
+		document.getElementsByClassName('blocks')[i].style.fill='black';
+	}
+
+	// -change block colour clicked block
+	// -change block colour next block
+	// -set original block onclick to be nothing
+	// -set next block onclick to be blocks
+}
